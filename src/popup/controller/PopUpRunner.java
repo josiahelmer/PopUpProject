132d6719e6aa894baeb40b2f7f091b0900819230
josiahelmer <jelm9149@740s-pl-112.local>
2015-09-24 12:24:28 -0600

    Starting up

---
+++ src/popup/controller/PopUpRunner.java
@@ -0,0 +1,13 @@
+package popup.controller;
+
+public class PopUpRunner
+{
+	public static void main (String [] args)
+	{
+		//Something mySomething = Something();
+		//mySomething.start();
+		PopUpController myPopUpCntroller =new PopUpController();
+		myPopUpController.start();
+	}
+
+}
